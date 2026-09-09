ea64b4ef4ad7fbd2eae98d5f8797d20c0c5798a8
vbsrl33010 <denis.giordano@vbsrl.it>
2026-09-09 09:15:11 +0000

    Aggiornato modello a gpt-oss-120b

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 
 url = "https://ia-rt.onrender.com/api/v1/chat"
-payload = {"message": "Ciao, mi confermi che funzioni?"}
+payload = {"message": "errore dgfe esurito"}
 
 print("Invio della richiesta all'assistente cloud...")
 response = requests.post(url, json=payload)
