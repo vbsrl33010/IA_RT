40999e3a6653c6d47f4ee8e0b9b0980b5c31b6e4
vbsrl33010 <denis.giordano@vbsrl.it>
2026-09-09 10:14:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ client = OpenAI(
     api_key=os.environ.get("GROQ_API_KEY")
 )
 
-MODEL_NAME = "llama-3.3-70b-versatile"
+MODEL_NAME = "llama-3.1-8b-instant"
 
 def vector_search(query: str) -> str:
     """Legge dinamicamente i manuali di testo presenti nella cartella knowledge_base"""
